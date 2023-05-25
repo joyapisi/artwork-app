db1c846856c47d5d7a67c3ec163035a688698237
Joy Phoebe <joyapisi@gmail.com>
2023-05-25 14:55:41 +0300

    🐛 Fx lnter errors

--- src/redux/home/homeSlice.js
+++ src/redux/home/homeSlice.js
@@ -7,7 +7,6 @@ export const fetchArtworks = createAsyncThunk('artworks/fetchArtworks', async ()
   try {
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data.data);
     return data.data;
   } catch (error) {
     return (error.message);
